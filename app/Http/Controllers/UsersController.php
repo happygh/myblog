2018887b5eda0bb33a8cdb08b9dc49f9847ac063
dongcaigh <dongcai5567@gmail.com>
2017-12-20 17:34:44 +0800

    finish in

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -135,4 +135,24 @@ class UsersController extends Controller
             $message->from($from, $name)->to($to)->subject($subject);
         });
     }
+
+    /**
+     * 显示关注的人
+     */
+    public function followings(User $user)
+    {
+        $users = $user->followings()->paginate(30);
+        $title = '关注的人';
+        return view('users.show_follow', compact('users', 'title'));
+    }
+
+    /**
+     * 显示粉丝
+     */
+    public function followers(User $user)
+    {
+        $users = $user->followers()->paginate(30);
+        $title = '粉丝';
+        return view('users.show_follow', compact('users', 'title'));
+    }
 }
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,24 +1,33 @@
 @extends('layouts.default')
 @section('title', $user->name)
 
-    @section('content')
-        <div class="row">
-            <div class="col-md-offset-2 col-md-8">
-                <div class="col-md-12">
-                    <div class="col-md-offset-2 col-md-8">
-                        <section class="user_info">
-                            @include('shared._user_info', ['user' => $user])
-                        </section>
-                    </div>
+@section('content')
+    <div class="row">
+        <div class="col-md-offset-2 col-md-8">
+            <div class="col-md-12">
+                <div class="col-md-offset-2 col-md-8">
+                    <section class="user_info">
+                        @include('shared._user_info', ['user' => $user])
+                    </section>
+                    <section class="stats">
+                        @include('shared._stats', ['user' => $user])
+                    </section>
                 </div>
-                @if(count($statuses) > 0)
+            </div>
+            <div class="col-md-12">
+                @if (Auth::check())
+                    @include('users._follow_form')
+                @endif
+
+                @if (count($statuses) > 0)
                     <ol class="statuses">
                         @foreach ($statuses as $status)
                             @include('statuses._status')
-                            @endforeach
+                        @endforeach
                     </ol>
                     {!! $statuses->render() !!}
-                    @endif
+                @endif
             </div>
         </div>
-        @stop
+    </div>
+@stop
